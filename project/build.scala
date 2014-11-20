6dbe5fb6092c244cdeab4e4149527dfbb9067810
wangxinli <wangxinli@derbysoft.com>
2014-11-20 17:50:14 +0800

    update log

--- project/build.scala
+++ project/build.scala
@@ -12,32 +12,32 @@ object ScalatraWebAppBuild extends Build {
   val ScalaVersion = "2.11.1"
   val ScalatraVersion = "2.3.0"
 
-  lazy val project = Project (
+  lazy val project = Project(
     "scalatra-web-app",
     file("."),
-    settings = ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
+    settings = Seq(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*) ++ ScalatraPlugin.scalatraFullSettings ++ scalateSettings ++ Seq(
       organization := Organization,
       name := Name,
       version := Version,
       scalaVersion := ScalaVersion,
       resolvers += Classpaths.typesafeReleases,
       libraryDependencies ++= Seq(
-        "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+        "org.scalatra" %% "scalatra" % ScalatraVersion exclude("org.slf4j", "slf4j-api"),
+        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion exclude("org.slf4j", "slf4j-api"),
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
+        "ch.qos.logback" % "logback-classic" % "1.1.2" % "compile",
         "org.eclipse.jetty" % "jetty-webapp" % "9.1.5.v20140505" % "container",
         "org.eclipse.jetty" % "jetty-plus" % "9.1.5.v20140505" % "container",
         "javax.servlet" % "javax.servlet-api" % "3.1.0"
       ),
-      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
+      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile) { base =>
         Seq(
           TemplateConfig(
             base / "webapp" / "WEB-INF" / "templates",
-            Seq.empty,  /* default imports should be added here */
+            Seq.empty, /* default imports should be added here */
             Seq(
               Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
-            ),  /* add extra bindings here */
+            ), /* add extra bindings here */
             Some("templates")
           )
         )
